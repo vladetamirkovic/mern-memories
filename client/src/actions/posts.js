5bd2049247009f9e8aed7d44c4caea82589ab930
vladetamirkovic <mirkovicvladeta2000@gmail.com>
2021-08-07 14:07:59 +0200

    added auth

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,19 +1,21 @@
-import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
-import * as api from '../api';
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+import * as api from '../api/index.js';
 
 export const getPosts = () => async (dispatch) => {
     try {
         const { data } = await api.fetchPosts();
+
         dispatch({ type: FETCH_ALL, payload: data });
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 };
 
 export const createPost = (post) => async (dispatch) => {
     try {
         const { data } = await api.createPost(post);
-        dispatch({ type: CREATE, payload: data })
+
+        dispatch({ type: CREATE, payload: data });
     } catch (error) {
         console.log(error);
     }
@@ -22,16 +24,20 @@ export const createPost = (post) => async (dispatch) => {
 export const updatePost = (id, post) => async (dispatch) => {
     try {
         const { data } = await api.updatePost(id, post);
-        dispatch({ type: UPDATE, payload: data })
+
+        dispatch({ type: UPDATE, payload: data });
     } catch (error) {
         console.log(error);
     }
 };
 
 export const likePost = (id) => async (dispatch) => {
+    const user = JSON.parse(localStorage.getItem('profile'));
+
     try {
-        const { data } = await api.likePost(id);
-        dispatch({ type: UPDATE, payload: data })
+        const { data } = await api.likePost(id, user?.token);
+
+        dispatch({ type: LIKE, payload: data });
     } catch (error) {
         console.log(error);
     }
@@ -39,8 +45,9 @@ export const likePost = (id) => async (dispatch) => {
 
 export const deletePost = (id) => async (dispatch) => {
     try {
-        await api.deletePost(id);
-        dispatch({ type: DELETE, payload: id })
+        await await api.deletePost(id);
+
+        dispatch({ type: DELETE, payload: id });
     } catch (error) {
         console.log(error);
     }
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
+import userRouter from "./routes/users.js";
 
 const app = express();
 dotenv.config();
@@ -14,6 +15,8 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use("/user", userRouter);
+
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
